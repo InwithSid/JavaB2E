50871482fe7bec480e5048117463a722c394a042
SidLabs <sidhartha.mitra@live.com>
2019-10-16 11:40:44 +0530

    This is Day#3_commit

---
+++ src/test2/Test2.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test2;
+
+/**
+ *
+ * @author Manpreet
+ */
+public class Test2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // a mile = 1.609344 kilometers 
+        int mile = 1;
+        double kilometers = (100 * 1.609344);
+        int highScore = 50;
+        
+        System.out.println("This is your high score :" + highScore +
+                        "\nyou have travelled " + kilometers + "kms till now" +
+                        "\ni.e. you have travelled " + mile +" mile!");
+              
+    }
+    
+}
