52abcea30730012ae660171a89f3ff5d3323310b
SidLabs <sidhartha.mitra@live.com>
2019-10-17 15:13:00 +0530

    This is Day#4_commit

--- src/test2/Assignment_1.java
+++ src/test2/Assignment_1.java
@@ -2,10 +2,21 @@
 package test2;
 
 public class Assignment_1 {
+
+    public String pt;
     
-    public String name = "Mustafa";
-    public int age = 24;
-    public String school = "Global School";
-    public int grade = 3;
-       
+    public String printAssign( String name, int age, String school, int grade){
+        
+        pt = "\nThis is the output of the Assignment_1Oct'19" +
+                            "\nName : " + name + "\nAge : " + age + "\nSchool : " + 
+                            school + "\nGrade : " + grade;
+        
+        return pt;
+    }
+    
+    public double calculateInterest (double ammount, double interestRate) {
+        return(ammount * (interestRate / 100));
+    }
+    
+    
 }
--- src/test2/Test2.java
+++ src/test2/Test2.java
@@ -14,22 +14,72 @@ public class Test2 {
     /**
      * @param args the command line arguments
      */
+   
     public static void main(String[] args) {
-        // a mile = 1.609344 kilometers 
-                
-        int mile = 1;
-        double kilometers = (100 * 1.609344);
-        int highScore = 50;
-        
-        Assignment_1 str = new Assignment_1();
-        
-        System.out.println("This is your high score :" + highScore +
-                        "\nyou have travelled " + kilometers + "kms till now" +
-                        "\ni.e. you have travelled " + mile +" mile!");
-        System.out.println("Name : " + str.name +"\nAge : " 
-                        + str.age + "\nSchool : "
-                        + str.school + "\nGrade : " + str.grade);
-              
+    Assignment_1 var = new Assignment_1();
+    System.out.println(" -------- Welcome to SidLabs ----------- ");
+
+    //int testValue = 2;
+    
+    for (int i=2; i<9; i++){
+        System.out.println("\n 10,000 at 2% is = " + String.format("%.2f", var.calculateInterest(10000.0, i)));
     }
     
-}
+    int count = 0;
+    
+    while(count != 9){
+        System.out.println("Count value is :" + count);
+        count++;
+    }
+    
+    count = 0;
+    
+    do {
+        System.out.println("Count value is :" + count);
+        count++;
+        }while (count != 9);   
+    
+    
+//    switch (testValue){
+//        case 1:
+//            System.out.println("\n the test value is 1");
+//            break;
+//        case 2:
+//            System.out.println("\n the test value is 2");
+//            break;
+//        case 3:
+//            System.out.println("\n the test value is 3");
+//            break;
+//        default:
+//            System.out.println("\n the test value is not in the range of 1 to 3 ");
+//            break;
+//    }
+//    
+    
+//    if (testValue == 1) {
+//        System.out.println("\n the test value is 1");
+//    } else if (testValue == 2){
+//        System.out.println("\n the test value is 2");
+//    } else if (testValue == 3){
+//        System.out.println("\n the test value is 3");
+//    } else {
+//        System.out.println("\n the test value is not in the range of 1 to 3 ");
+//    }
+//    
+   
+    
+    
+    
+    
+    
+//    
+//        System.out.println("\n" + var.printAssign("Geeta", 24, "Global School", 3) );
+//        
+//        
+    }
+          
+    
+    }
+
+
+   
