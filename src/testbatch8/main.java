4be77d903018ca0c5607f152f8ec22835f3fa98d
InwithSid <sidhartha.mitra@live.com>
2019-11-01 00:22:46 +0530

    31st oct commit

--- src/testbatch8/main.java
+++ src/testbatch8/main.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package testbatch8;
-
+import java.util.Scanner;
 /**
  *
  * @author Administrator
@@ -16,18 +16,84 @@ public class main {
      */
     public static void main(String[] args) {
         
-        System.out.println("************ Welcome to SidLabs *****************\n\n");
+    System.out.println("************ Welcome to SidLabs *****************\n\n");
+    String num1;
+    double num2;
+    String num3;
+    String num4;
+    String num5;
+    
+    Scanner scn = new Scanner(System.in);
+    
+    //take the input
+    
+        System.out.println("Please enter the Customer Number :");
+        num1 = scn.nextLine();
+        System.out.println("Please enter the opening Balance :");
+        num2 = scn.nextDouble();
+        System.out.println("Please enter the Customer Name :");
+        num3 = scn.nextLine();
+        System.out.println("Please enter the Customer Email :");
+        num4 = scn.nextLine();
+        System.out.println("Please enter the Customer Phone :");
+        num5 = scn.nextLine();
+    
+    Account acn = new Account(num1, num2, num3, num4, num5);
+    
+        System.out.println("The account balance is " + acn.getBalance());
+        System.out.println("The account Name is " + acn.getCustomerName());
+        
+        acn.deposit(12000.00);
+        acn.withdrawal(2546.345);
+        
+        
+//    VipAccount vip = new VipAccount("32112", 5000.00, "Avijeet", "Avi@iva.com", "1234343");
+//        
+//    System.out.println("The account's credit limit is " + vip.creditLimit);
+//        System.out.println("The account category is " + vip.category);
+//        
+//        vip.deposit(12000.00);
+//        vip.withdrawal(23000.20);
+//        
+//        System.out.println(vip.toString());
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //
-        String string = "This is my test string which I am testing";
-        int count = 0;
-        char y = ' ';
-        for (int i=0; i < string.length(); i++){
-            if(string.charAt(i)!= y){
-                count++;
-                
-        }
-            }
-            System.out.println("The total number of charecters in this string is " + count);
+        //String string = "This is my test string which I am testing";
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter a text string to test : ");
+//        String str = sc.nextLine();
+//        int count = 0;
+//        char y = ' ';
+//        for (int i=0; i < str.length(); i++){
+//            if(str.charAt(i)!= y){
+//                count++;
+//                
+//        }
+//            }
+//            System.out.println("\n\tTotal number of charecters in this string : " + count + "\n\n");
     
         
         
