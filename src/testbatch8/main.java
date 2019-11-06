98781f2b6226b5eb69ced245ac64efae1eb709e3
InwithSid <sidhartha.mitra@live.com>
2019-11-06 19:45:58 +0530

    5th Nov commit

--- src/testbatch8/main.java
+++ src/testbatch8/main.java
@@ -4,7 +4,10 @@
  * and open the template in the editor.
  */
 package testbatch8;
-//import java.util.Scanner;
+import java.util.Scanner;
+
+
+
 /**
  *
  * @author Administrator
@@ -17,115 +20,33 @@ public class main {
     public static void main(String[] args) {
         
     System.out.println("************ Welcome to SidLabs *****************\n\n");
+
+    int row, col, i, j;
+    int var[][] = new int[10][10];
     
-    //Billing System in java
-    
-    ItemData obj = new ItemData();
-    
-        obj.PrintItems();
-        //System.out.println(obj.getItems());
-    
-    
-    
-    
-//    String num1;
-//    double num2;
-//    String num3;
-//    String num4;
-//    String num5;
-//    
-//    Scanner scn = new Scanner(System.in);
-//    
-//    //take the input
-//    
-//        System.out.println("Please enter the Customer Number :");
-//        num1 = scn.nextLine();
-//        System.out.println("Please enter the opening Balance :");
-//        num2 = scn.nextDouble();
-//        System.out.println("Please enter the Customer Name :");
-//        num3 = scn.nextLine();
-//        System.out.println("Please enter the Customer Email :");
-//        num4 = scn.nextLine();
-//        System.out.println("Please enter the Customer Phone :");
-//        num5 = scn.nextLine();
-//    
-//    Account acn = new Account(num1, num2, num3, num4, num5);
-//    
-//        System.out.println("The account balance is " + acn.getBalance());
-//        System.out.println("The account Name is " + acn.getCustomerName());
-//        
-//        acn.deposit(12000.00);
-//        acn.withdrawal(2546.345);
+        System.out.println("\nEnter the size of Rows : ");
+        Scanner scn = new Scanner(System.in);
+        row = scn.nextInt();
+        System.out.println("\nEnter the size of Columns : ");
+        col = scn.nextInt();
         
+            System.out.println("Enter "+row*col+" number of list items");
+            for (i=0; i<row; i++){
+                for (j=0; j<col; j++){
+                      var[i][j] = scn.nextInt();
+                }
+            }
         
-//    VipAccount vip = new VipAccount("32112", 5000.00, "Avijeet", "Avi@iva.com", "1234343");
-//        
-//    System.out.println("The account's credit limit is " + vip.creditLimit);
-//        System.out.println("The account category is " + vip.category);
-//        
-//        vip.deposit(12000.00);
-//        vip.withdrawal(23000.20);
-//        
-//        System.out.println(vip.toString());
+        System.out.println("\n Your list of items are ,");
+            for (i=0; i<row; i++){
+                for (j=0; j<col; j++){
+                      System.out.print(var[i][j]+"\t");
+                }
+                System.out.println("");
+            }
         
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
-        //String string = "This is my test string which I am testing";
-//        Scanner sc = new Scanner(System.in);
-//        System.out.println("Enter a text string to test : ");
-//        String str = sc.nextLine();
-//        int count = 0;
-//        char y = ' ';
-//        for (int i=0; i < str.length(); i++){
-//            if(str.charAt(i)!= y){
-//                count++;
-//                
-//        }
-//            }
-//            System.out.println("\n\tTotal number of charecters in this string : " + count + "\n\n");
     
-        
-        
-        
-                
-
-
-
-
-//        // a mile = 1.609344 kilometers
-//        
-//        int mile = 1;
-//        double highscore = 100.12;
-//        double kilometers = 100 * 1.609344;
-//        String str = "SidLabs";
-//        
-//        
-//        System.out.println("This is your highscore :" + highscore + "\n" + "You have travelled a " + mile);
-//        
-//        
-//        
+       
     }
     
 }
